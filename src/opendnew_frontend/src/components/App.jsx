5f2876aabcfd043988204562c937961188e2d169
giorgi Kapanadze <giorgikapanadze@giorgis-MacBook-Pro.local>
2023-03-05 19:40:49 +0000

    connect backend with HttpAgent and Createactor

--- src/opendnew_frontend/src/components/App.jsx
+++ src/opendnew_frontend/src/components/App.jsx
@@ -4,12 +4,15 @@ import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import homeimg from "../../assets/homeimg.png"
+import Item from "./Item";
 
 function App() {
+
   return (
     <div className="App">
       <Header />
-      <img className="bottom-space" src={homeimg} />
+      <Item />
+      {/* <img className="bottom-space" src={homeimg} /> */}
       <Footer />
     </div>
   );
